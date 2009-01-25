5d6c5e21834e3bc9168721653adf7998aeba5d34
Vyacheslav Kononenko <vyacheslav@kononenko.net>
2009-01-25 04:40:26 +0000

    svn:keywords properites added

--- cmake/FindLightScribe.cmake
+++ cmake/FindLightScribe.cmake
@@ -16,7 +16,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
-#    $Id: $ 
+#    $Id$ 
 
 FIND_PATH( LSCRIBE_INCLUDE_DIR lightscribe.h /usr/local/include )
 
