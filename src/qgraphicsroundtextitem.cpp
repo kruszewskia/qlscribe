c039ab4bb60646b78e5ce4fddd32c094b001eccf
Vyacheslav Kononenko <vyacheslav@kononenko.net>
2009-01-27 01:18:25 +0000

    Round text reading bug fixed (did not read angle)

--- src/qgraphicsroundtextitem.cpp
+++ src/qgraphicsroundtextitem.cpp
@@ -16,7 +16,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-    $Id:$ */
+    $Id$ */
 
 #include "qgraphicsroundtextitem.h"
 #include "qdialogroundtext.h"
@@ -284,6 +284,11 @@ void QShapeControllerRoundText::readData( const QString &element,
 
    if( element == "location" ) {
       textItem->setOutside( data.toInt() );
+      return;
+   }
+
+   if( element == "angle" ) {
+      textItem->setAngle( data.toDouble() );
       return;
    }
 
