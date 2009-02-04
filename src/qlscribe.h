b0817a1108f2c68569ad561f0fc658ad41ba4afb
Vyacheslav Kononenko <vyacheslav@kononenko.net>
2009-02-04 19:30:38 +0000

    header added for projectwise includes

---
+++ src/qlscribe.h
@@ -0,0 +1,33 @@
+/*  qlscribe - Qt based application to print lightScribe discs
+
+    Copyright (C) 2009 Vyacheslav Kononenko <vyacheslav@kononenko.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    $Id:$ */
+
+#ifndef QLSCRIBE_H
+#define QLSCRIBE_H
+
+enum LabelMode {
+   /** label the entire disc */
+   modeFull=0,
+   /** label within the title mode constraints */
+   modeTitle=1,
+   /** label within the content mode constraints */
+   modeContent=2
+};
+
+#endif // QLSCRIBE_H
