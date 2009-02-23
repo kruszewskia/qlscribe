b2141aa8f888b53a46f7fbf12446ffe81b1a96d5
Vyacheslav Kononenko <vyacheslav@kononenko.net>
2009-02-23 22:50:34 +0000

    DBus search improved

--- cmake/FindDbus32.cmake
+++ cmake/FindDbus32.cmake
@@ -25,15 +25,14 @@ IF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
         FIND_LIBRARY( DBUS_LIBRARY dbus-1 
                       PATHS /lib32 /usr/lib32 /usr/local/lib32
                       NO_DEFAULT_PATH )
-ELSE( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
-        FIND_LIBRARY( DBUS_LIBRARY dbus-1 )
 ENDIF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" )
 
+FIND_LIBRARY( DBUS_LIBRARY dbus-1 )
 
 get_filename_component( _dbusLibPath ${DBUS_LIBRARY} PATH )
 
 FIND_PATH( DBUS_LIBINCLUDE_DIR dbus/dbus-arch-deps.h
-           PATHS /lib /usr/lib /usr/local/lib
+           PATHS /lib /usr/lib /usr/local/lib ${_dbusLibPath}
            PATH_SUFFIXES dbus-1.0/include )
 
 if( DBUS_INCLUDE_DIR AND DBUS_LIBINCLUDE_DIR AND DBUS_LIBRARY )
