d9f223757c19fe4933b621199bb962baacce7784
Vyacheslav Kononenko <vyacheslav@kononenko.net>
2010-01-19 02:43:00 +0000

    changes for release 0.14

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -539,7 +539,7 @@ void MainWindow::onMenuAbout()
    QMessageBox::about( this,
                        tr( "About" ),
                        tr( "<h3>qlscribe - Qt lisghtScribe</h3>"
-                           "<p>prerelease 0.14 $Revision$</p>"
+                           "<p>release 0.14 $Revision$</p>"
                            "<p>visit project at home page "
                            "<a href=\"http://qlscribe.sourceforge.net/\">qlscribe.sourceforge.net</a></p>" ) );
 }
